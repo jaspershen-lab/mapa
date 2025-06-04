108f86fb1cf47f4c4a1304e178e4be81181e4965
GriffyGe <yifeii.ge@outlook.com>
2025-06-04 16:47:44 +0800

    Fixed bug.

--- R/16_llm_interpret_module.R
+++ R/16_llm_interpret_module.R
@@ -8,13 +8,12 @@
 # Metabolite
 # llm_interpreted_enriched_functional_module <-
 #   llm_interpret_module(
-#     object = enriched_functional_module,
+#     object = object,
 #     llm_model = "gpt-4o-mini-2024-07-18",
 #     embedding_model = "text-embedding-3-small",
 #     api_key = api_key,
-#     phenotype = "pregnancy",
-#     embedding_output_dir = "demo_data/pregnancy_data/results/results_biotext/embedding_output/"
-#   )
+#     embedding_output_dir = "demo_data/debug_yijiang/embedding_ouput/"
+# )
 
 # Gene
 # llm_interpreted_functional_module <-
